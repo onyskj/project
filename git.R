bc23f5ed7b466cf079504432ca475be27a7c6560
Jane Doe <onysk.jakub@gmail.com>
2022-08-31 12:44:07 +0000

    Initial commit

---
+++ git.R
@@ -0,0 +1,6 @@
+#use a function without loading the package
+# package::function
+usethis::use_git_config(
+  user.name = "Jane Doe",
+  user.email = "onysk.jakub@gmail.com",
+  init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
